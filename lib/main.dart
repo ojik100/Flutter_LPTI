f0e95cbf5a033fb90a176dabbb61e1adeace7188
MOH.FAHRURROZI <56944631+ojik100@users.noreply.github.com>
2020-12-01 06:18:28 -0800

    Lpti

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Pertama LPTI"),
+        ),
+        body: Center(
+          child: Text("Hello Word"),
+        ),
+      ),
+    );
+  }
+}
